2f0db5f15e27161b5a285c3c0d39abf451d26971
Ivan Lenkavets <47rex47@mail.ru>
2023-10-20 17:04:44 +0300

    add description for plugin

--- ldd-map.php
+++ ldd-map.php
@@ -2,4 +2,14 @@
 
 /*
  * Plugin Name: Карта для сайта ldd.by
+ * Description:Карта для сайта ldd.by
+ * Author:      Иван Ленковец
+ * Version:     1.0
+ *
+ * Requires at least: 2.5
+ * Requires PHP: 5.4
+ *
+ * License:     GPL2
+ *
+ * Network:     Укажите "true" для возможности активировать плагин для сети Multisite.
  */

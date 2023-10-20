203002ff010453fb9c7d39fe806a63a07b1031d6
Ivan Lenkavets <47rex47@mail.ru>
2023-10-20 17:14:05 +0300

    fix description

--- ldd-map.php
+++ ldd-map.php
@@ -1,15 +1,78 @@
-?php
+<?php
 
-/*
- * Plugin Name: Карта для сайта ldd.by
- * Description:Карта для сайта ldd.by
- * Author:      Иван Ленковец
- * Version:     1.0
+/**
+ * The plugin bootstrap file
  *
- * Requires at least: 2.5
- * Requires PHP: 5.4
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
  *
- * License:     GPL2
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           Plugin_Name
  *
- * Network:     Укажите "true" для возможности активировать плагин для сети Multisite.
+ * @wordpress-plugin
+ * Plugin Name:       ldd-map
+ * Description:       Карта для сайт ldd.by
+ * Version:           1.0.0
+ * Author:            Иван Ленковец
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_plugin_name' );
+register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
+}
+run_plugin_name();
